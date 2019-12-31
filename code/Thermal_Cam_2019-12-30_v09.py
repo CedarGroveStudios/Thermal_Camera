344a68599733607e9f2eb60ef468f45fe72ba04d
CedarGroveStudios <jan@goolsbey.net>
2019-12-30 23:50:33 -0800

    Update Thermal_Cam_2019-12-30_v09.py

--- code/Thermal_Cam_2019-12-30_v09.py
+++ code/Thermal_Cam_2019-12-30_v09.py
@@ -49,6 +49,9 @@ panel.play_tone(880, 0.1)  # A5
 WIDTH  = board.DISPLAY.width
 HEIGHT = board.DISPLAY.height
 
+MIN_SENSOR_C = 0
+MAX_SENSOR_C = 80
+
 ELEMENT_SIZE = 16
 BLACK   = 0x000000
 RED     = 0xFF0000
@@ -69,20 +72,21 @@ element_color = [GRAY, BLUE, GREEN, YELLOW, ORANGE, RED, VIOLET, WHITE]
 
 ### Converters and Helpers ###
 def convert_temp(f=None, c=None):  # convert F to C and C to F
-    if f != None and c == None: return round((f - 32) * (5 / 9), 1)
-    if f == None and c != None: return int(round(((9 / 5) * c) + 32, 0))
+    if f != None and c == None: return round((f - 32) * (5 / 9))
+    if f == None and c != None: return round(((9 / 5) * c) + 32)
     return None
 
 def element_grid(row, col):
     return Coords(ELEMENT_SIZE * row + 30, ELEMENT_SIZE * col + 1)
 
 def update_image_frame():
-    minimum = 80 # set minimum to maximum C value
-    maximum = 0  # set maximum to minimum C value
+    minimum = MAX_SENSOR_C # set minimum to sensor's maximum C value
+    maximum = MIN_SENSOR_C  # set maximum to sensor's minimum C value
     sum_bucket = 0  # bucket for building average value
     for row in range(0, 8):  # parse camera data list and update display
         for col in range(0, 8):
-            value = map_range(image[7 - col][7 - row], 0, 80, 0, 80)
+            value = map_range(image[7 - col][7 - row],
+                            MIN_SENSOR_C, MAX_SENSOR_C, MIN_SENSOR_C, MAX_SENSOR_C)
             pos = element_grid(row, col)
             color_index = int(map_range(value, MIN_RANGE_C, MAX_RANGE_C, 0, 7))
             disp_group[((row * 8) + col) + 1] = Rect(x=pos.x, y=pos.y, width=ELEMENT_SIZE,
@@ -94,13 +98,14 @@ def update_image_frame():
     return minimum, maximum, sum_bucket
 
 def update_histo_frame():
-    minimum = 80 # set minimum to maximum C value
-    maximum = 0  # set maximum to minimum C value
+    minimum = MAX_SENSOR_C # set minimum to sensor's maximum C value
+    maximum = MIN_SENSOR_C  # set maximum to sensor's minimum C value
     sum_bucket = 0  # bucket for building average value
     histo_bucket = [0, 0, 0, 0, 0, 0, 0, 0]  # reset histogram bucket
     for row in range(0, 8):  # parse camera data list and update display
         for col in range(7, -1, -1):
-            value = map_range(image[7 - col][7 - row], 0, 80, 0, 80)
+            value = map_range(image[7 - col][7 - row],
+                              MIN_SENSOR_C, MAX_SENSOR_C, MIN_SENSOR_C, MAX_SENSOR_C)
             histo_index = int(map_range(value, MIN_RANGE_C, MAX_RANGE_C, 0, 7))
             histo_bucket[histo_index] = histo_bucket[histo_index] + 1
             pos = element_grid(histo_index, 7 - (histo_bucket[histo_index] // 8))
@@ -111,24 +116,55 @@ def update_histo_frame():
             sum_bucket = sum_bucket + value  # calculate sum for average
             minimum = min(value, minimum)
             maximum = max(value, maximum)
+
     return minimum, maximum, sum_bucket
 
-def setup_mode(alarm):
+def setup_mode():
+    disp_group[65].text  = "-SET-"
+    disp_group[71].text = str(MAX_RANGE_F)
+    disp_group[72].text = str(MIN_RANGE_F)
+    disp_group[69].color = BLACK
+    disp_group[73].color = BLACK
+
+    parameter_index = 0  # index of parameter to set
+
     while not panel.button.select:
-        disp_group[65].color = BLACK
-        disp_group[65].text  = "-SET-"
-        disp_group[66].color = BLACK
-        time.sleep(0.1)
         left, right, up, down = move_buttons(joystick=panel.has_joystick)
-        if up  : alarm = alarm + 1
-        if down: alarm = alarm - 1
-        if alarm > MAX_RANGE_F: alarm = MAX_RANGE_F
-        if alarm < MIN_RANGE_F: alarm = MIN_RANGE_F
-        disp_group[70].text = str(int(alarm))
+        if up  : parameter_index = parameter_index - 1
+        if down: parameter_index = parameter_index + 1
+        if parameter_index > 2: parameter_index = 2
+        if parameter_index < 0: parameter_index = 0
+        disp_group[parameter_index + 66].color = BLACK
+        disp_group[65].color = BLACK
+        time.sleep(0.3)
+        disp_group[parameter_index + 66].color = WHITE
         disp_group[65].color = WHITE
-        disp_group[66].color = WHITE
+        time.sleep(0.3)
+    while panel.button.select:  # wait for button release
         time.sleep(0.1)
-    return alarm
+
+    print(parameter_index)
+    parameter_value = int(disp_group[parameter_index + 70].text)
+    print(parameter_value)
+    while not panel.button.select:
+        left, right, up, down = move_buttons(joystick=panel.has_joystick)
+        if up  :  parameter_value = parameter_value + 1
+        if down:  parameter_value = parameter_value - 1
+        if parameter_value > convert_temp(c=MAX_SENSOR_C):  parameter_value = convert_temp(c=MAX_SENSOR_C)
+        if parameter_value < convert_temp(c=MIN_SENSOR_C):  parameter_value = convert_temp(c=MIN_SENSOR_C)
+        disp_group[parameter_index + 70].text = str(parameter_value)
+        disp_group[parameter_index + 66].color = GREEN
+        disp_group[65].color = BLACK
+        time.sleep(0.1)
+        disp_group[parameter_index + 66].color = RED
+        disp_group[65].color = WHITE
+        time.sleep(0.1)
+    while panel.button.select:  # wait for button release
+        time.sleep(0.1)
+
+    disp_group[65].color = BLACK
+    time.sleep(0.1)
+    return int(disp_group[70].text), int(disp_group[71].text),int(disp_group[72].text)
 
 def move_buttons(joystick=False):
     if joystick:  # for PyGamer
@@ -146,7 +182,7 @@ def move_buttons(joystick=False):
     return move_r, move_l, move_u, move_d
 
 ### Set alarm and range values ###
-ALARM_F     = 120               # alarm temp in Farenheit
+ALARM_F     = 120                      # alarm temp in Farenheit
 ALARM_C     = convert_temp(f=ALARM_F)  # alarm temp in Celsius
 MIN_RANGE_F =  50
 MAX_RANGE_F = 120
@@ -158,6 +194,7 @@ disp_group = displayio.Group(max_size=74)
 board.DISPLAY.show(disp_group)
 
 # Create a background color fill
+# disp_group[0]
 color_bitmap = displayio.Bitmap(WIDTH, HEIGHT, 1)
 color_palette = displayio.Palette(1)
 color_palette[0] = BLACK
@@ -166,6 +203,7 @@ background = displayio.TileGrid(color_bitmap, pixel_shader=color_palette,
 disp_group.append(background)
 
 # Define and build the foundational display group
+# disp_group[1:64]
 for row in range(0, 8):
     for col in range(0, 8):
         pos = element_grid(col, row)
@@ -173,50 +211,59 @@ for row in range(0, 8):
                           width=ELEMENT_SIZE, height=ELEMENT_SIZE,
                           fill=None))
 
+# disp_group[65]
 hold_label = Label(font, text="", color=WHITE, max_glyphs=6)
 hold_label.x = (WIDTH // 2) - 7
 hold_label.y = 15 + int(1.4 * HEIGHT // 4)
 disp_group.append(hold_label)
 
+# disp_group[66]
 alarm_label = Label(font, text="alm", color=WHITE, max_glyphs=3)
 alarm_label.x = 0
 alarm_label.y = 10 + int(0.5 * HEIGHT // 4)
 disp_group.append(alarm_label)
 
+# disp_group[67]
 max_label = Label(font, text="max", color=RED, max_glyphs=3)
 max_label.x = 0
 max_label.y = 10 + int(1.5 * HEIGHT // 4)
 disp_group.append(max_label)
 
-ave_label = Label(font, text="ave", color=YELLOW, max_glyphs=3)
-ave_label.x = 0
-ave_label.y = 10 + int(2.5 * HEIGHT // 4)
-disp_group.append(ave_label)
-
+# disp_group[68]
 min_label = Label(font, text="min", color=CYAN, max_glyphs=3)
 min_label.x = 0
 min_label.y = 10 + int(3.5 * HEIGHT // 4)
 disp_group.append(min_label)
 
+# disp_group[69]
+ave_label = Label(font, text="ave", color=YELLOW, max_glyphs=3)
+ave_label.x = 0
+ave_label.y = 10 + int(2.5 * HEIGHT // 4)
+disp_group.append(ave_label)
+
+# disp_group[70]
 alarm_display = Label(font, text=str(ALARM_F), color=WHITE, max_glyphs=5)
 alarm_display.x = 0
 alarm_display.y = 10 + int(0 * HEIGHT // 4)
 disp_group.append(alarm_display)
 
+# disp_group[71]
 max_display = Label(font, text="176", color=RED, max_glyphs=5)
 max_display.x = 0
 max_display.y = 10 + int(1 * HEIGHT // 4)
 disp_group.append(max_display)
 
-ave_display = Label(font, text="---", color=YELLOW, max_glyphs=5)
-ave_display.x = 0
-ave_display.y = 10 + int(2 * HEIGHT // 4)
-disp_group.append(ave_display)
-
+# disp_group[72]
 min_display = Label(font, text="32", color=CYAN, max_glyphs=5)
 min_display.x = 0
 min_display.y = 10 + int(3 * HEIGHT // 4)
 disp_group.append(min_display)
+
+# disp_group[73]
+ave_display = Label(font, text="---", color=YELLOW, max_glyphs=5)
+ave_display.x = 0
+ave_display.y = 10 + int(2 * HEIGHT // 4)
+disp_group.append(ave_display)
 
 time.sleep(1.0)
 disp_group[65].text = ""
@@ -235,8 +282,8 @@ while True:  # Display image or histogram
     # update display text values
     disp_group[70].text = str(ALARM_F)
     disp_group[71].text = str(convert_temp(c=v_max))
-    disp_group[72].text = str(convert_temp(c=v_sum // 64))
-    disp_group[73].text = str(convert_temp(c=v_min))
+    disp_group[72].text = str(convert_temp(c=v_min))
+    disp_group[73].text = str(convert_temp(c=v_sum // 64))
 
     if v_max >= ALARM_C:  panel.play_tone(880, 0.030)  # A5
 
@@ -265,7 +312,7 @@ while True:  # Display image or histogram
     if panel.button.start:  # activate setup mode (setup mode = start button)
         while panel.button.b:  pass  # wait for button release
         panel.play_tone(784, 0.030)  # G5
-        ALARM_F = setup_mode(ALARM_F)
+        ALARM_F, MAX_RANGE_F, MIN_RANGE_F = setup_mode()
         ALARM_C = convert_temp(f=ALARM_F)  # alarm temp in Celsius
         MIN_RANGE_C = convert_temp(f=MIN_RANGE_F)
         MAX_RANGE_C = convert_temp(f=MAX_RANGE_F)
